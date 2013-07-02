c7dd95b5578e8ab0076daa120f7b11c05676027a
John Lyle <john.lyle@cs.ox.ac.uk>
2013-07-02 16:29:26 +0100

    Invitations improvement: automatically redirect to the invitees PZH web server.
    Jira issue: WP-852

--- public/js/get-pzh-info.js
+++ public/js/get-pzh-info.js
@@ -1,10 +1,21 @@
-/*
-window.onload = function() {
-	var box = document.getElementById("pzhWebUrl");
-	box.value = webinos.session.getPzhWebAddress();
-}
-*/
-
+/*******************************************************************************
+ *  Code contributed to the webinos project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright 2013 The University of Oxford
+ * Author: John Lyle <john.lyle@cs.ox.ac.uk>
+ *******************************************************************************/
 window.onload = function() {
 
   var channel;
@@ -27,7 +38,8 @@ window.onload = function() {
 	  console.log("Data received from the PZP: " + JSON.stringify(data));
 	  if (data && data.payload && data.payload.message && data.payload.message.pzhWebAddress) {
 	  	var box = document.getElementById("pzhWebUrl");
-		box.value = data.payload.message.pzhWebAddress;
+		  box.value = data.payload.message.pzhWebAddress;
+      document.getElementById("inviteRedirectForm").submit();
 	  }
   }
   channel.onopen = function() {
